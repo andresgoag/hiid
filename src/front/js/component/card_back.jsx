07a58a1e6d8ddb62d563d436ea9c12af3cb7da9d
David Gómez <davidgoag1@gmail.com>
2021-12-01 15:50:07 +0000

    CSS re-organized in files

--- src/front/js/component/card_back.jsx
+++ src/front/js/component/card_back.jsx
@@ -9,11 +9,11 @@ export const CardBack = props => {
 
 		<div className="center-v">
 			<div className="">
-				<figure className="company-logo-back">
+				<figure className="company-logo-back gap-t-20">
 					<img src="https://gghtec.com/wp-content/uploads/2021/11/CompanyLogoHiid.png" alt="" />
 				</figure>
 				<div className="company-logo-back gap-t-20">
-					<p className="last-name-typography">{props.slogan}</p>
+					<p className="last-name-typography custom-p">{props.slogan}</p>
 				</div>
 			</div>
 		</div>
